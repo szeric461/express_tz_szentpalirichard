f258920a6c9618d86c959704e490c867c5953a21
szeric461 <93657723+szeric461@users.noreply.github.com>
2025-04-10 13:50:07 +0200

    kész

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    port: 3306,
+    password: "",
+    database: "fogado"
+})
+
+app.get("/", (req, res) => {
+    res.send("Fut a backend.");
+});
+
+app.get("/szobak", (req, res) => {
+    const sql = "SELECT * FROM szobak";
+    db.query(sql, (err, result) => {
+        res.json(result);
+    });
+});
+
+app.listen(3000, () => {
+    console.log("3000-es porton fut.");
+});
